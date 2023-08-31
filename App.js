16f623dab94766d40cabdc7c6864367bd5f16c3c
huiciraul <cesarhuici1@gmail.com>
2023-08-31 16:29:47 -0300

    primer commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import { LogBox } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import Toast from "react-native-toast-message"
+import { AppNavigation } from "./src/navigation/AppNavigation"
+LogBox.ignoreAllLogs();
+export default function App() {
+  return (
+    <>
+      <NavigationContainer>
+         <AppNavigation/>
+      </NavigationContainer>
+
+      <Toast/>
+    </>
+  );
+}
+
